42103eb5bd9328b7b9c095f573573fdff0bda4d1
Spencer Firlik <safirlik@mtu.edu>
2020-02-13 09:20:29 -0500

    Fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	has_many :bugs, dependent: :destroy
 
-	validates :lname, :fname, :email, presence: true { message: "Field must not be blank" }
+	validates :lname, :fname, :email, presence: { message: "Field must not be blank" }
 	validates :email, uniqueness: {case_sensitive: false}, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "Not a valid email address"}
 	validates :thumbnail, allow_blank: true, format: {with: %r{\.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG, or PNG image.'}
 end
