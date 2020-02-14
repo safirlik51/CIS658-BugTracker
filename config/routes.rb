26563f3bc996b85b7ee5ce9add89eaa49028f6d5
Spencer Firlik <safirlik@mtu.edu>
2020-02-13 20:32:36 -0500

    Fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users
   resources :bugs
-  root 'home#index'
+  root 'bugs#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
