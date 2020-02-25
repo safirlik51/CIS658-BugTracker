5f9ba5bb1c654eea1d8682bf449f54fb85d712bd
Spencer Firlik <safirlik@mtu.edu>
2020-02-25 10:12:28 -0500

    postValidate.js

---
+++ app/javascript/packs/postValidate.js
@@ -0,0 +1,12 @@
+ let form = document.getElementById('bugForm');
+ let errorMessage = document.getElementById('errorMessage'); 
+
+ form.addEventListener('submit', (event) => {
+   let titleText = document.getElementsByName('bug[title]')[0].value;
+   let descriptionText = document.getElementsByName('bug[description]')[0].value;
+
+   if (titleText.length == 0 || descriptionText.length == 0) {
+   	errorMessage.innerHTML = "Please Enter a Title and Description!";
+	event.preventDefault();
+   }
+ });
