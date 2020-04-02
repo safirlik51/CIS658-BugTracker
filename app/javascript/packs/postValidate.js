1d670b7c8b83d1db52bd235e76162c99c7bc5740
Spencer Firlik <safirlik@mtu.edu>
2020-04-02 15:10:28 -0400

    Fixed Bug Class

--- app/javascript/packs/postValidate.js
+++ app/javascript/packs/postValidate.js
@@ -2,11 +2,14 @@
  let errorMessage = document.getElementById('errorMessage'); 
 
  form.addEventListener('submit', (event) => {
-   let titleText = document.getElementsByName('bug[title]')[0].value;
-   let descriptionText = document.getElementsByName('bug[description]')[0].value;
-
-   if (titleText.length == 0 || descriptionText.length == 0) {
-   	errorMessage.innerHTML = "Please Enter a Title and Description!";
-	event.preventDefault();
-   }
+	let titleText = document.getElementsByName('bug[title]')[0].value;
+	let descriptionText = document.getElementsByName('bug[description]')[0].value;
+   
+	if (titleText.length == 0 || descriptionText.length == 0) {
+   		errorMessage.innerHTML = "Please Enter a Title and Description!";
+		event.preventDefault();
+	}
+	else{
+   		errorMessage.innerHTML = '';
+	}
  });
